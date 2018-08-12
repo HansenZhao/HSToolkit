092b19d652702dc3149474ce832428746220245a
HansenZhao <zhaohs12@mails.tsinghua.edu.cn>
2018-08-12 13:22:39 +0800

    support regulate expression

--- listFile.m
+++ listFile.m
@@ -9,19 +9,28 @@ function [fileName,filePath] = listFile(spec,path)
         return;
     end
     files = mat2cell(files,ones(1,L));
-    fileType = strsplit(spec,'.');
-    if strcmp(fileType{1},'*')
+    index = strfind(spec,'.');
+    matchName = spec(1:(index(end)-1));
+    if strcmp(matchName,'*')
         matchName = [];
-    else
-        matchName = fileType{1};
     end
-    fileType = fileType{2};
-    if strcmp(fileType,'*')
-        matchFormat = [];
-    else
-        matchFormat = fileType;
-    end
-    for m = 3:1:L
+    matchFormat = spec((index(end)+1:end));
+%     fileType = strsplit(spec,'.');
+%     if strcmp(fileType{1},'*')
+%         matchName = [];
+%     else
+%         matchName = fileType{1};
+%     end
+%     fileType = fileType{2};
+%     if strcmp(fileType,'*')
+%         matchFormat = [];
+%     else
+%         matchFormat = fileType;
+%     end
+    for m = 1:1:L
+        if files{m}(1) == '.'
+            continue;
+        end
         if isdir(strcat(path,'\',files{m}))
             [fn,fp] = listFile(spec,strcat(path,'\',files{m}));
             len = size(fn,1);
@@ -33,12 +42,20 @@ function [fileName,filePath] = listFile(spec,path)
             try
                 tmp = files{m};
                 tmp = strtrim(tmp);
-                tmp = strsplit(tmp,'.');
-                fName = tmp{1};
-                fFormat = tmp{2};
+                I = strfind(tmp,'.');
+                fName = tmp(1:(I(end)-1));
+                fFormat = tmp((I(end)+1):end);
+%                 tmp = strsplit(tmp,'.');
+%                 if length(tmp) == 2
+%                     fName = tmp{1};
+%                     fFormat = tmp{2};
+%                 elseif length(tmp) > 2
+%                     fFormat = tmp{end};
+%                     fName = strcat(tmp{1:(end-1)});
+%                 end
                 if isempty(matchFormat) || strcmp(matchFormat,fFormat)
                     %if strcmp(name,spec)
-                    if regexp(fName,matchName)
+                    if isempty(matchName) || regexp(fName,matchName)
                         fileName{end+1} = files{m};
                         filePath{end+1} = strcat(path,'\');
                     end
